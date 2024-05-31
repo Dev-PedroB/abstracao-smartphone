52025da31641fed09fd14ad93f301e35cbc39991
Pedro Braga <154244665+Dev-PedroB@users.noreply.github.com>
2024-05-31 11:45:00 -0300

    Implementacoes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de um objeto Iphone
+            Console.WriteLine("SMARTPHONE IPHONE");
+            Smartphone iphone = new Iphone("123456789", "iPhone 12", "123456789012345", 128);
+            iphone.Ligar();
+            iphone.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Criação de um objeto Nokia
+            Console.WriteLine("SMARTPHONE NOKIA");
+            Smartphone nokia = new Nokia("987654321", "Nokia 3310", "543210987654321", 32);
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Telegram");
+        }
+    }
+}
